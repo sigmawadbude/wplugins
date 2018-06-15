3ca7814888d17d8db5c245472266a35d72a716db
Sigma wadbude <sigmawadbude@gmail.com>
2018-06-15 11:22:09 +0530

    new Plugin added which add output content to page headers using wp_head hook

---
+++ ch2-page-header-output/ch2-page-header-output.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Plugin Name: Page Header Output
+Plugin URI: 
+Description: Companion to recipe 'Adding output content to page headers using plugin actions'
+Version: 1.0.0
+Author: SigMA Wadbude <sigma.wadbude@gmail.com>
+Author URI: 
+License: GPLv2
+*/
+
+/* 
+Copyright (C) 2018 SigMA Wadbude <sigma.wadbude@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+add_action( 'wp_head', 'ch2pho_page_header_output' );
+
+function ch2pho_page_header_output(){ ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-XXXXX-Y', 'auto');
+        ga('send', 'pageview');
+    </script>
+<?php }
